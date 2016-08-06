e8c72bd2e8ea914b4e0e2d4bdad4a166feeb4388
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-07 04:03:58 +0800

    上傳

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+const port = process.env.PORT || 80;
+
+app.get('/taichung', (req, res) => {
+    request({ url: 'http://ybjson01.youbike.com.tw:1002/gwjs.json' }, (error, response, body) => {
+        res.send(response);
+    });
+});
+
+app.listen(port, () => { console.log('listening on port ' + port); });
