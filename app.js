743f95e2d1d7af5efd85ed1e8e6ca1910662eeec
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-07 04:11:53 +0800

    允許CORS

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const request = require('request');
 const port = process.env.PORT || 80;
 
+app.use((req, res, next) => { res.header('Access-Control-Allow-Origin', '*'); });
+
 app.get('/taichung', (req, res) => {
     request({ url: 'http://ybjson01.youbike.com.tw:1002/gwjs.json' }, (error, response, body) => {
         res.send(response);
