cb54febf5ae385c5b6c3485b649777fae8a3ce32
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-07 20:13:23 +0800

    更新express版本and修正錯誤

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 const request = require('request');
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => { res.header('Access-Control-Allow-Origin', '*'); });
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 
-app.get('/taichung', (req, res) => {
-    request({ url: 'http://ybjson01.youbike.com.tw:1002/gwjs.json' }, (error, response, body) => {
+app.get('/taichung', function (req, res) {
+    console.log('get');
+    request({ url: 'http://ybjson01.youbike.com.tw:1002/gwjs.json' }, function (error, response, body) {
         res.send(response);
+        // console.log(response);
     });
 });
 
-app.listen(port, () => { console.log('listening on port ' + port); });
+app.listen(port, function () { console.log('listening on port ' + port); });
