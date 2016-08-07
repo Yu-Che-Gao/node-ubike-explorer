2b01c7f84d0adae80c7164ad06199432a6265e6b
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-07 20:14:40 +0800

    更新port and 語法

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const app = express();
 const request = require('request');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
 });
 
-app.get('/taichung', function (req, res) {
-    console.log('get');
-    request({ url: 'http://ybjson01.youbike.com.tw:1002/gwjs.json' }, function (error, response, body) {
+app.get('/taichung', (req, res) => {
+    request({ url: 'http://ybjson01.youbike.com.tw:1002/gwjs.json' }, (error, response, body) => {
         res.send(response);
         // console.log(response);
     });
 });
 
-app.listen(port, function () { console.log('listening on port ' + port); });
+app.listen(port, () => { console.log('listening on port ' + port); });
