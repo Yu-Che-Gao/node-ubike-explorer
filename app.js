20e560bbdb338c2834e8275f22641cfab9a0f6a9
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-17 17:44:23 +0800

    微調

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ app.use(function (req, res, next) {
   next();
 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', (ws) => {
   var location = url.parse(ws.upgradeReq.url, true);
-  ws.on('message', function incoming(message) {
+  ws.on('message', (message) => {
     console.log('received: %s', message);
   });
-  
+
   ws.send('this is something');
 });
 
 server.on('request', app);
-server.listen(port, function () { console.log('Listening on ' + server.address().port) });
+server.listen(port, () => { console.log('Listening on ' + server.address().port) });
